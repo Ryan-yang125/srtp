1ca6e990bda038fb7ff22d5efe5d2dfbf7058e95
Ryan-yang125 <1598094295@qq.com>
2021-05-24 00:08:15 +0800

    finish

---
+++ Assets/Scripts/Visual.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+
+
+[Serializable]
+public class Visual
+{
+    public string datas;
+}
